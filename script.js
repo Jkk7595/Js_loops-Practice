9de21f3b23d76714eb2ef3ab7ac2b71e6f1e88b4
Jkk7595 <jospeterkananu7595@gmail.com>
2025-11-17 09:53:42 +0300

    :

--- script.js
+++ script.js
@@ -1,4 +1,41 @@
-let i = 1
-for(i;i<=20; i++){
-    console.log(i)
-}
+
+        // Count from 1 to 20 (For Loop)
+
+// let i = 1
+// for(i;i<=20; i++){
+//     console.log(i)
+// }
+
+
+        // Sum of Numbers 1–50 (While Loop)
+
+// index =1
+// sum = 0
+// while(index<=100){
+//   sum += index;
+//   index++
+// }
+// console.log(sum)
+
+
+    // // Print Each Item in an Array (for…of Loop)
+    // let fruits = ['Mango', 'Apple', 'Banana', 'Orange'];
+    // for(let fruit of fruits ){
+    //     console.log(fruit)
+    //     // console.log(fruits[0],fruits[1],fruits[2],fruits[3])
+    // }
+
+
+        // Even Numbers Between 2 and 40 (For Loop)   
+        // i = 2
+        
+        // for (i;i<=40;i+=2){
+        //     console.log(` Even number:${i}`)
+        // }
+        
+        i = 2
+        for (i;i<=40;){
+         if (i % 2 === 0) {
+        console.log(i);
+        }
+    }
